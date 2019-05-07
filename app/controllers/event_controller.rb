cde0b1f4e529ae22d0960c047496904e9a5f58f4
klud <vincentgillibert@gmail.com>
2019-05-08 00:06:36 +0200

    Add more views

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -9,6 +9,7 @@ class EventController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @end_date = self.end_date
   end
 
   def create
@@ -26,11 +27,18 @@ class EventController < ApplicationController
     if @event.save # essaie de sauvegarder en base @event
       # si ça marche, il redirige vers la page d'index du site
       puts "Tu as réussi"
-      redirect_to event_index_path
+      render :show
     else
       # sinon, il render la view new (qui est celle sur laquelle on est déjà)
       puts "ca ne fontionne pas"
       render new_event_path
     end
+  end
+
+  def end_date
+    event_start = Event.find(params[:id]).start_date
+    event_duration = Event.find(params[:id]).duration
+
+    return event_start + (event_duration*60*60)
   end
 end
