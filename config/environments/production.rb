cf8d1234d45a54798fa00e37e92070517d0d8f13
klud <vincentgillibert@gmail.com>
2019-05-07 17:33:41 +0200

    Pimp forms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Customs
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'klud-eventbrite-like.herokuapp.com' }
 end
