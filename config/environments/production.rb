d5fda97229a80b0c4c006d845ac4abd214408ad7
klud <vincentgillibert@gmail.com>
2019-05-07 16:13:46 +0200

    fix bugs for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Customs
+  config.action_mailer.default_url_options = { host: 'klud-eventbrite-like.herokuapp.com' }
 end
