3657d0f65d4d69f7d3c382de9c261ba07281dcba
klud <vincentgillibert@gmail.com>
2019-05-07 15:17:11 +0200

    fix mailer bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,16 +60,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Customs
-  config.action_mailer.default_url_options = { host: 'klud-eventbrite-like.herokuapp.com' }
-
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_LOGIN'],
-    :password => ENV['SENDGRID_PWD'],
-    :domain => 'monsite.fr',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
